97334624b9bf2318901bb2c701df2addc63e8f51
Ade Suluh <ade.suluh@gmail.com>
2020-10-02 21:33:23 +0700

    add simple contact router

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ func main() {
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Someone visit our web")
-	fmt.Fprint(w, "<h1>Hello dopefaceee, trying bro!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Println("Someone visit our web")
+		fmt.Fprint(w, "<h1>Hello dopefaceee, trying bro!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send me an email to<a href=\"mailto:ade.suluh@gmail.cp,\">ade.suluh@gmail.com</a>")
+
+	}
+
 }
