9b5530369bab1adc1ebe79c39d7c8a64232e8db8
Ade Suluh <ade.suluh@gmail.com>
2020-10-18 01:23:29 +0700

    add navbar and add to layout

--- views/view.go
+++ views/view.go
@@ -2,8 +2,12 @@ package views
 
 import "html/template"
 
+// NewView for abstraction create a view and layout
 func NewView(layout string, files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
+	files = append(files,
+		"views/layouts/navbar.gohtml",
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
